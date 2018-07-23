1cc75bdfcf4264230f016f5201bc4d74821c3502
mertgurcan <mertgurcan@icloud.com>
2018-07-23 03:24:03 +0300

    add Darksky and opencagedata api functionality

--- HavaHava/Controller/MainScreen.swift
+++ HavaHava/Controller/MainScreen.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class MainScreen: UIViewController , UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,20 +28,14 @@ class MainScreen: UIViewController , UITableViewDelegate, UITableViewDataSource,
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
     }
     
-    
-        override var prefersStatusBarHidden: Bool{
-            return true
-        }
-   
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
+    // MARK: - Screen
     // Screen width.
     public var screenWidth: CGFloat {
         return UIScreen.main.bounds.width
@@ -48,7 +45,7 @@ class MainScreen: UIViewController , UITableViewDelegate, UITableViewDataSource,
     public var screenHeight: CGFloat {
         return UIScreen.main.bounds.height
     }
-    
+    // MARK: - Tableview
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 4
@@ -92,17 +89,49 @@ class MainScreen: UIViewController , UITableViewDelegate, UITableViewDataSource,
             }
         return 10
     }
+
+    // MARK: - Location
     
-    
-    
-    /*
-     // MARK: - Navigation
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
      
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+        }
+        
+        getWeather(lat: String(location.coordinate.latitude), lon: String(location.coordinate.longitude))
+        
+    }
     
+    // MARK: - Weather
+   
+   
+    func getWeather(lat: String, lon: String){
+        
+        let url_string = Connections.init().WEATHER_URL + "\(Connections.init().WEATHER_APP_ID)/\(lat),\(lon)"
+        
+        Alamofire.request(url_string, method: .get).responseJSON{
+            response in
+            if response.result.isSuccess{
+                let weatherResult: JSON = JSON(response.result.value!)
+                
+                Alamofire.request(Connections.init().LOC_URL + "q=\(lat)+\(lon)&key=\(Connections.init().LOC_APP_ID)").responseJSON{
+                    response in
+                    if response.result.isSuccess{
+                        let locResult: JSON = JSON(response.result.value!)
+                        print(locResult)
+                        print(weatherResult)
+                    }
+                    else{
+                        print("Error \(String(describing: response.result.error)) occured")
+                    }
+                }
+            }
+            else{
+                print("Error \(String(describing: response.result.error)) occured")
+            }
+        }
+        
+        
+    }
 }
