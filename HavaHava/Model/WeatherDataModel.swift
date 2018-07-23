a973226a4da82a65546461f891b980ae05ab4bd9
mertgurcan <mertgurcan@icloud.com>
2018-07-23 04:14:40 +0300

    add WeatherDataModel class

---
+++ HavaHava/Model/WeatherDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherDataModel.swift
+//  HavaHava
+//
+//  Created by Mert Gürcan on 23.07.2018.
+//  Copyright © 2018 Mert Gürcan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherDataModel {
+
+
+}
